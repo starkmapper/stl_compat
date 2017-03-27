2bbf7d5653ae8c49b82fbaf957f5fcb336204fd8
Mark Stapper <mark.stapper@rhmarinegroup.com>
2017-03-27 17:58:29 +0200

    Add thread include

---
+++ include/std_compat/threading/thread.hpp
@@ -0,0 +1,61 @@
+#ifndef threadHPP
+#define threadHPP
+#include "../helper/cppversion_detection.hpp"
+
+#ifdef cpp11
+   #include <thread>
+  #define thread_namespace std
+#else
+  #include <boost/thread.hpp>
+  #define thread_namespace boost
+namespace boost
+{
+  namespace this_thread
+  {
+    template <class TimeDuration>
+    void sleep_for(TimeDuration const& rel_time)
+    {
+      sleep(rel_time);
+    }
+  }
+}
+#endif
+
+namespace std_compat
+{
+  using thread_namespace::thread;
+  using thread_namespace::swap;
+  using thread_namespace::operator==;
+  using thread_namespace::operator!=;
+  using thread_namespace::operator<;
+  using thread_namespace::operator<=;
+  using thread_namespace::operator>;
+  using thread_namespace::operator>=;
+  using thread_namespace::operator<<;
+  
+  namespace this_thread
+  {
+    using namespace thread_namespace::this_thread;
+  }
+}
+
+#if !cpp11
+namespace std
+{
+  using std_compat::thread;
+  using std_compat::swap;  
+  using std_compat::operator==;
+  using std_compat::operator!=;
+  using std_compat::operator<;
+  using std_compat::operator<=;
+  using std_compat::operator>;
+  using std_compat::operator>=;
+  using std_compat::operator<<;
+  namespace this_thread
+  {
+    using namespace std_compat::this_thread;
+  }
+}
+#endif
+
+#endif
---
+++ include/std_compat/utilities/chrono.hpp
@@ -0,0 +1,31 @@
+#ifndef chronoHPP
+#define chronoHPP
+#include "../helper/cppversion_detection.hpp"
+
+#if cpp11
+  #include <chrono>
+  #define chrono_namespace std::chrono
+#else
+  #include <boost/date_time/posix_time/posix_time_types.hpp>
+  #define chrono_namespace boost::posix_time
+#endif
+
+namespace std_compat
+{
+  namespace chrono
+  {
+    using namespace chrono_namespace;
+  }
+}
+
+#if !cpp11
+namespace std
+{
+  namespace chrono
+  {
+    using namespace std_compat::chrono;
+  }
+}
+#endif
+
+#endif
