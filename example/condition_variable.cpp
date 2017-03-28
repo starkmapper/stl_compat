9ec8976a1d51fe3461196ba1de64f9d22424fc8a
Mark Stapper <mark.stapper@rhmarinegroup.com>
2017-03-28 10:00:44 +0200

    Add wait_for example to condition_variable example

--- example/condition_variable.cpp
+++ example/condition_variable.cpp
@@ -43,6 +43,37 @@ public:
     bool &value;
   bool operator()() { return value; }
 };
+
+
+std_compat::condition_variable cv2;
+std::mutex cv_m;
+int i;
+
+void waits(int idx)
+{
+  std::unique_lock<std::mutex> lk(cv_m);
+  if (cv2.wait_for(lk, std::chrono::milliseconds(idx * 100), [] {return i == 1; }))
+    std::cerr << "Thread " << idx << " finished waiting. i == " << i << '\n';
+  else
+    std::cerr << "Thread " << idx << " timed out. i == " << i << '\n';
+}
+
+void signals()
+{
+  std::this_thread::sleep_for(std::chrono::milliseconds(120));
+  std::cerr << "Notifying...\n";
+  cv2.notify_all();
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  {
+    std::lock_guard<std::mutex> lk(cv_m);
+    i = 1;
+  }
+  std::cerr << "Notifying again...\n";
+  cv2.notify_all();
+}
+
+
+
 int main()
 {
   std::thread worker(worker_thread);
@@ -64,4 +95,8 @@ int main()
   std::cout << "Back in main(), data = " << data << '\n';
 
   worker.join();
+  std::cout << "Running wait_for" << std::endl;
+  
+  std::thread t1(waits, 1), t2(waits, 2), t3(waits, 3), t4(signals);
+  t1.join(); t2.join(), t3.join(), t4.join();
 }
