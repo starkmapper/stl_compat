a60727758604ae20c898ea8285c3c1237c444e44
Chris Werkmeester <chris.werkmeester@rhmarine.com>
2017-04-06 11:16:14 +0200

    Fixed array.hpp.

--- include/std_compat/containers/array.hpp
+++ include/std_compat/containers/array.hpp
@@ -1,13 +1,23 @@
-#ifndef ArrayH
-#define ArrayH
-#include "../cppversion_detection.hpp"
-#include <array>
-namespace std_compat
-{
-  #if CPP11
-  using std::array;
-  #else
-  using boost::array;
-  #endif
-}
+#ifndef std_compat_arrayH
+#define std_compat_arrayH
+#include "../helper/cppversion_detection.hpp"
+
+#if cpp11
+  #include <array>
+  namespace std_compat
+  {
+    using std::array;
+  }
+#else
+  #include <boost/array.hpp>
+  namespace std_compat
+  {
+    using boost::array;
+  }
+  namespace std
+  {
+    using boost::array;
+  }
+#endif
+
 #endif
