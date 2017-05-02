24f70bcda7b9688382b2fdfa0d4222de195b84fb
Chris Werkmeester <chris.werkmeester@rhmarine.com>
2017-05-02 09:23:05 +0200

    Added std::bind.

---
+++ include/std_compat/utilities/bind.hpp
@@ -0,0 +1,23 @@
+#ifndef std_compat_bindH
+#define std_compat_bindH
+#include "../helper/cppversion_detection.hpp"
+
+#if cpp11
+#include <bind>
+namespace std_compat
+{
+  using std::bind;
+}
+#else
+#include <boost/bind.hpp>
+namespace std_compat
+{
+  using boost::bind;
+}
+namespace std
+{
+  using boost::bind;
+}
+#endif
+
+#endif
