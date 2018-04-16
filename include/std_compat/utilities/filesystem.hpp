16d20cc3824d8b780bd7690024a8ed33aaaf6aa1
Chris Werkmeester <chris.werkmeester@rhmarine.com>
2018-04-16 13:28:04 +0200

    #32756 Added std_compat/filesystem

---
+++ include/std_compat/utilities/filesystem.hpp
@@ -0,0 +1,44 @@
+#ifndef std_compat_filesystemH
+#define std_compat_filesystemH
+#include "../helper/cppversion_detection.hpp"
+
+#if cpp17
+namespace std_compat
+{
+  namespace filesystem
+  {
+    using namespace std::filesystem;
+  }
+}
+#else
+#include <boost/filesystem.hpp>
+
+namespace std_compat
+{
+  namespace filesystem
+  {
+    using namespace boost::filesystem;
+  }
+}
+namespace std
+{
+  namespace filesystem
+  {
+    using namespace boost::filesystem;
+
+    path absolute(const path& p)
+    {
+      return boost::filesystem::system_complete(p);
+    }
+
+    //path absolute(const path& p, std::error_code& ec)
+    template <typename T>
+    path absolute(const path& p, T& ec)
+    {
+      static_assert(false, "Not Supported in boost");
+    }
+  }
+}
+#endif
+
+#endif
